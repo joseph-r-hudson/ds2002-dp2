8404027ae4889ca24c6768cf79c387ee245cca5e
Joseph Hudson <rck7ye@virginia.edu>
2024-05-03 17:51:02 +0000

    edits

--- project2script.py
+++ project2script.py
@@ -19,20 +19,8 @@ directory = "data"
 
 collection_name ="json_data"
 
+
 good_docs = 0
-docs_not_imported = 0
-corrupt_docs = 0
-
-
-def check_json(json_data):
-   try:
-       # Attempt to load JSON data
-       json.loads(json_data)
-       return True
-   except json.JSONDecodeError:
-       # If JSON decoding fails, return False
-       return False
-
 
 for filename in os.listdir(directory):
    try:
@@ -43,20 +31,5 @@ for filename in os.listdir(directory):
            collection.insert_many(new_data)
    except Exception as e:
         print(f"Error decoding JSON in file {filename}: {e}")
-        with open(os.path.join(directory, filename)) as f:
-           broken_file = f.read()
-           documents = broken_file.strip("[").strip("]").split('"_id"')
-           documents.pop(0)
-           for i in documents:
-                strip = i.strip(" ").strip("'").strip("{").strip(",")
-                id_fixed = '{"_id"'+strip
-                if id_fixed.endswith("}"):
-                    docs_not_imported+=1
-                else:
-                   corrupt_docs+=1
 
 
-              
-print(docs_not_imported)
-print(corrupt_docs)
-print(good_docs)
